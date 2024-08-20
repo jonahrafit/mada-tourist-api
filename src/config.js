ab934519f9d7d9b87e684341da816ff5df044a6e
jonahrafit <89374053+jonahrafit@users.noreply.github.com>
2024-08-20 15:08:31 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const MONGO_URI = 'mongodb://localhost:27017/madaTourDB';
+export const MONGO_URI = 'mongodb://192.168.88.22:27017/madaTourDB';
 export const PORT = 5001;
 export const MAIL = 'jonahrafit.ram@hotmail.com';
 export const MAIL_PASSWORD = 'ETU1156fitiapok1:3';
